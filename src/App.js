517881cd22f3146a3a2cc5e99b031a76bb0674af
Akinola Daniel Oshinubi <87362118+danieloshinubi@users.noreply.github.com>
2024-03-15 00:49:39 -0300

    testing out the dropdown

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Landing from "./Components/Main/Landing";
 import Navbar from "./Components/Navbar/Navbar";
+// import DropDownMenu from "./Components/Navbar/DropDownMenu";
 function App() {
   return (
     <Router className="App">
       <Navbar />
       <Landing />
+
     </Router>
   );
 }
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -4,19 +4,19 @@ import "../../css/Navbar.css";
 import { Link, useLocation } from "react-router-dom";
 
 function Navbar() {
-  const [isDropdownVisible, setDropdownVisible] = useState(false);
-  const handleMouseEnter = () => {
-    setDropdownVisible(true);
-  };
+  const [dropDown, setDropdown] = useState(false);
+//   const handleMouseEnter = () => {
+//     // setDropdownVisible(true);
+//   };
 
-  const handleMouseLeave = () => {
-    setDropdownVisible(false);
-  };
+//   const handleMouseLeave = () => {
+//     // setDropdownVisible(false);
+//   };
   const navigationLinks = [
     {
       path: "/Features",
       text: " Features ",
-      icon: (
+      arrow: (
         <svg width="10" height="6" xmlns="http://www.w3.org/2000/svg">
           <path
             stroke="#686868"
@@ -24,20 +24,14 @@ function Navbar() {
             fill="none"
             d="m1 1 4 4 4-4"
           />
-          <path
-            stroke="#686868"
-            stroke-width="1.5"
-            fill="none"
-            d="m1 5 4-4 4 4"
-            style={{ display: "none" }}
-          />
         </svg>
       ),
+      className: "nav-item",
     },
     {
       path: "/Company",
       text: " Company ",
-      icon: (
+      arrow: (
         <svg width="10" height="6" xmlns="http://www.w3.org/2000/svg">
           <path
             stroke="#686868"
@@ -47,9 +41,10 @@ function Navbar() {
           />
         </svg>
       ),
+      className: "nav-item",
     },
-    { path: "/Careers", text: " Careers " },
-    { path: "/About", text: " About " },
+    { path: "/Careers", text: " Careers ",className:'nav-item' },
+    { path: "/About", text: " About ",className:'nav-item' },
   ];
   const navigationLinksLogin = [
     { path: "/Login", text: " Login " },
@@ -57,65 +52,106 @@ function Navbar() {
   ];
 
   return (
-    <nav className="Navbar">
-      <div className="snap">
-        <h1 style={{ fontSize: "40px" }}>snap</h1>
-        <ul className="snap-ul">
-          {navigationLinks.map((link) => (
-            <li key={link.path} className="nav-menu">
-              <Link
-                to={link.path}
-                className="snap-nav-link hover-stuff"
+    <>
+      <nav className="Navbar">
+        <div className="snap">
+          <h1 style={{ fontSize: "40px" }}>snap</h1>
+          <ul className="snap-ul">
+            {navigationLinks.map((link) => {
+              if (link.path === "/Features") {
+                return (
+                  <li
+                    key={link.path}
+                    className="nav-menu"
+                    onMouseEnter={() => {
+                      setDropdown(true);
+                    }}
+                    onMouseLeave={() => {
+                      setDropdown(false);
+                    }}
+                  >
+                    <Link
+                      to={link.path}
+                      className="snap-nav-link hover-stuff"
+                      style={{ textDecoration: "none" }}
+                    >
+                      {link.text}
+                    </Link>
+                    {link.arrow && link.arrow}
+                    {dropDown && <DropDownMenu />}
+                  </li>
+                );
+              }
+            //   else if(link.path==="/Company"){
+            //     return(<li key={link.path} className="nav-menu">
+            //     <Link
+            //       to={link.path}
+            //       className="snap-nav-link hover-stuff"
+            //       style={{ textDecoration: "none" }}
+            //     >
+            //       {link.text}
+            //     </Link>
+            //     {link.arrow && link.arrow}
+            //     {dropDown && <DropDownMenu />}
+            //   </li>)
+            //   }
+              return (
+                <li key={link.path} className="nav-menu">
+                  <Link
+                    to={link.path}
+                    className="snap-nav-link hover-stuff"
+                    style={{ textDecoration: "none" }}
+                  >
+                    {link.text}
+                  </Link>
+                  {link.arrow && link.arrow}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <div className="login">
+          <ul className="login-ul">
+            {navigationLinksLogin.map((log) => (
+              <li
+                key={log.path}
+                className="nav-menu register hover-stuff"
                 style={{ textDecoration: "none" }}
               >
-                {link.text}
-              </Link>
-              {link.icon && link.icon}
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className="login">
-        <ul className="login-ul">
-          {navigationLinksLogin.map((log) => (
-            <li
-              key={log.path}
-              className="nav-menu register hover-stuff"
-              style={{ textDecoration: "none" }}
-            >
-              {log.path === "/Register" ? (
-                <Link
-                  to={log.path}
-                  className="hover-stuff"
-                  style={{
-                    textDecoration: "none",
-                  }}
-                  onMouseEnter={handleMouseEnter}
-                  onMouseLeave={handleMouseLeave}
-                >
-                  {log.text}
-                  {isDropdownVisible && <DropDownMenu />}
-                </Link>
-              ) : (
-                <Link
-                  to={log.path}
-                  className="hover-stuff"
-                  style={{ textDecoration: "none" }}
-                >
-                  {log.text}
-                </Link>
-              )}{" "}
-            </li>
-          ))}
-        </ul>
-        <div className="hamburger">
-          {" "}
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-            <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
-          </svg>
+                {log.path === "/Register" ? (
+                  <Link
+                    to={log.path}
+                    className="hover-stuff"
+                    style={{
+                      textDecoration: "none",
+                    }}
+                    //   onMouseEnter={handleMouseEnter}
+                    //   onMouseLeave={handleMouseLeave}
+                  >
+                    {log.text}
+                    {/* {isDropdownVisible && <DropDownMenu />} */}
+                  </Link>
+                ) : (
+                  <Link
+                    to={log.path}
+                    className="hover-stuff"
+                    style={{ textDecoration: "none" }}
+                  >
+                    {log.text}
+                  </Link>
+                )}{" "}
+              </li>
+            ))}
+          </ul>
+          <div className="hamburger">
+            {" "}
+            <svg xmlns="http://www.w3.org/2000 /svg" viewBox="0 0 448 512">
+              <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
+            </svg>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 }
 
